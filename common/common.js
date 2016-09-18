0f90477ad02986bb77779abe757de5a44aa4ff85
micro <648580211@qq.com>
2016-09-18 15:01:17 +0800

    Update common.js

--- common/common.js
+++ common/common.js
@@ -6,3 +6,24 @@ function getParamByName (name, url) {
 
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+function getQueryString(params) {
+    var queryString = window.location.href;
+    var regex = new RegExp(name + "=[^&#]*");
+
+    for (var i in params) {
+        if (params.hasOwnProperty(i)) {
+            if (queryString.match(new RegExp(i + '='))) {
+                regex = new RegExp(i + "=[^&#]*");
+                queryString = queryString.replace(regex, i + '=' + params[i]);
+            } else {
+                if (queryString.split('?')[1] && queryString.split('?')[1].length) {
+                    queryString += '&' + i + '=' + params[i];
+                } else {
+                    queryString += '?' + i + '=' + params[i];
+                }
+            }
+        }
+    }
+    return queryString;
+}
